e44d522411365ddbbfc2a32ee91d2fca42d3f7ef
chengcheng <cc5092@gmail.com>
2016-02-01 15:46:34 +0800

    up

--- test/server.test.js
+++ test/server.test.js
@@ -1,31 +1,49 @@
-var should = require('should');
-var Server = require('../lib/server');
-var Client = require('../lib/client');
-var net = require('net');
-var ExBuffer = require('ExBuffer');
-var ByteBuffer = require('ByteBuffer');
-var Connection = require('../lib/connection');
+'use strict';
+const should = require('should');
+const Server = require('../lib/server');
+const Client = require('../lib/client');
+const net = require('net');
+const ExBuffer = require('ExBuffer');
+const ByteBuffer = require('ByteBuffer');
+const Connection = require('../lib/connection');
 
 describe('server', function() {
-  var port = 3000;
-  it('server must listen', function(done) {
-    var server = new Server(port);
-    server.on('listening', function() {
+  const port = 3000;
+
+  it('server must listen', done => {
+    let server = new Server(port);
+    server.on('listening', () => {
+      server.close();
       done();
     });
   });
 
-  it('client must connect', function(done) {
-    var server = new Server(port);
-    var client = new Client(port);
-    done();
+  it('client must connect', done => {
+    let server = new Server(port);
+    let client = new Client(port);
+    client.on('connection', connection => {
+      connection.should.be.a.instanceof(Connection);
+      client.end();
+      server.close();
+      done();
+    });
+
+  });
+
+  it('client must send', done => {
+    let server = new Server(port);
+    let client = new Client(port);
+    let msg = {
+      cmd: 'cmd',
+      data: 'abcd'
+    }
+    client.send(msg)
+    server.on('data', data => {
+      console.log(data);
+      client.end();
+      server.close();
+      done();
+    });
+
   });
 });
-/*
-describe('connection', function() {
-  it('connection function', function() {
-    var s = {};
-    var c = new Connection(s, s)
-    c.should.be.an.instanceof('Connection');
-  });
-})*/
